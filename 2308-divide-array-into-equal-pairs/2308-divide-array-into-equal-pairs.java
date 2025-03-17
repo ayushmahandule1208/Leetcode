4b7bd8037ff76357ba3efd8b26b708552d4d1e60
Ayush Chandrakant Mahandule <ayushmahandule1208@gmail.com>
2025-03-17 09:10:29 +0530

    Time: 4 ms (74.89%), Space: 44.4 MB (58.82%) - LeetHub

--- 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.java
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.java
@@ -2,17 +2,16 @@ import java.util.Arrays;
 
 class Solution {
     public boolean divideArray(int[] nums) {
-        if (nums.length % 2 != 0) return false; // Odd length can't be divided into pairs
+        if (nums.length % 2 != 0) return false; 
         
-        Arrays.sort(nums); // Sort the array
+        Arrays.sort(nums);
 
         for (int i = 0; i < nums.length; i += 2) {
-            // Check if adjacent elements are equal
             if (nums[i] != nums[i + 1]) {
                 return false;
             }
         }
 
-        return true; // If all pairs are equal
+        return true;
     }
 }
