03e27b0c735a629c47d58b814e226e276a24c1b9
Ayush Chandrakant Mahandule <ayushmahandule1208@gmail.com>
2025-12-21 11:02:26 +0530

    Time: 2 ms (93.64%), Space: 48 MB (17.41%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int n = nums.length;
+        int[] count = new int[n+1];
+        int[] res = new int[2];
+
+        for(int num:nums){
+            count[num]++;
+        }
+
+        for(int i=1;i<=n;i++){
+            if(count[i]==2) res[0] = i;
+            if(count[i]==0) res[1] = i;
+        }
+        return res;
+    }
+}
