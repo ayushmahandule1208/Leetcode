7e84e6cdfa34fe00d3b8340023d721846ca81cbb
Ayush Chandrakant Mahandule <ayushmahandule1208@gmail.com>
2026-02-14 10:30:21 +0530

    Time: 4 ms (80.12%), Space: 46.5 MB (33.16%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,10 +3,18 @@ class Solution {
         char[] sArr = s.toCharArray();
         char[] tArr = t.toCharArray();
 
+        if(sArr.length != tArr.length){
+            return false;
+        }
+
         Arrays.sort(sArr);
         Arrays.sort(tArr);
 
-        return Arrays.equals(sArr,tArr);
-        
+        for(int i =0;i<sArr.length;i++ ){
+            if(sArr[i]!=tArr[i]){
+                return false;
+            }
+        }
+        return true;
     }
 }
